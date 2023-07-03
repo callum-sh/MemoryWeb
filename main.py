397314ff2cd95715b567a1587759c60d8931285b
Callum Sharrock <38599348+callum-sh@users.noreply.github.com>
2023-07-03 15:53:13 -0400

    here

--- main.py
+++ main.py
@@ -1,114 +1,36 @@
 import datetime
-from google.auth.exceptions import RefreshError
-from googleapiclient.discovery import build
 import networkx as nx
-import plotly.graph_objects as go
-import random
-
-import os
-import pandas as pd
-from google.oauth2.credentials import Credentials
+import requests
 from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-
-# Define the edges and vertices
-edges = [(1, 2), (1, 3), (2, 3), (2, 4), (3, 4)]
-vertices = [1, 2, 3, 4]
-
-# Create a graph
-G = nx.Graph()
-G.add_nodes_from(vertices)
-G.add_edges_from(edges)
-
-# Generate positions for the nodes
-pos = nx.spring_layout(G)
-
-edge_trace = go.Scatter(x=[], y=[], line=dict(
-    width=0.5, color='#888'), hoverinfo='none', mode='lines')
-
-for edge in G.edges():
-    x0, y0 = pos[edge[0]]
-    x1, y1 = pos[edge[1]]
-    edge_trace['x'] += tuple([x0, x1, None])
-    edge_trace['y'] += tuple([y0, y1, None])
-
-node_trace = go.Scatter(x=[], y=[], mode='markers', hoverinfo='text', marker=dict(
-    showscale=True, colorscale='YlGnBu', reversescale=True, color=[], size=10))
-
-for node in G.nodes():
-    x, y = pos[node]
-    node_trace['x'] += tuple([x])
-    node_trace['y'] += tuple([y])
-
-# Add node colors to the node trace
-for node, adjacencies in enumerate(G.adjacency()):
-    node_trace['marker']['color'] += tuple([len(adjacencies[1])])
-
-fig = go.Figure(data=[edge_trace, node_trace],
-                layout=go.Layout(title='<br>Network graph',
-                showlegend=False,
-                hovermode='closest',
-                margin=dict(b=20, l=5, r=5, t=40),
-                xaxis=dict(showgrid=False, zeroline=False,
-                           showticklabels=False),
-                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)))
-
-fig.show()
-
-
-def get_photos():
-    scopes = ['https://www.googleapis.com/auth/photoslibrary.readonly']
-
-    creds = None
-
-    if os.path.exists('_secrets_/token.json'):
-        creds = Credentials.from_authorized_user_file(
-            '_secrets_/token.json', scopes)
-
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                '_secrets_/client_secret.json', scopes)
-            creds = flow.run_local_server()
-        print(creds)
-        # Save the credentials for the next run
-        with open('_secrets_/token.json', 'w') as token:
-            token.write(creds.to_json())
-
 
 # Google Photos API information
 SCOPES = ['https://www.googleapis.com/auth/photoslibrary.readonly']
-API_SERVICE_NAME = 'photoslibrary'
-API_VERSION = 'v1'
 
 
 def service_account_login():
-    creds = None
-
-    # Load credentials from the 'credentials.json' file
     flow = InstalledAppFlow.from_client_secrets_file(
         'credentials.json', SCOPES)
     creds = flow.run_local_server(port=8080)
 
-    try:
-        # Build the service
-        service = build(API_SERVICE_NAME, API_VERSION, credentials=creds)
-        return service
-    except RefreshError:
-        print("Failed to connect.")
-        return None
+    return creds
 
 
-def get_all_photos(service):
-    results = service.mediaItems().list(pageSize=100).execute()
-    items = results.get('mediaItems', [])
+def get_all_photos(creds):
+    headers = {
+        'Authorization': 'Bearer {}'.format(creds.token)
+    }
 
-    while 'nextPageToken' in results:
-        results = service.mediaItems().list(
-            pageSize=100, pageToken=results['nextPageToken']).execute()
-        items.extend(results.get('mediaItems', []))
+    response = requests.get(
+        'https://photoslibrary.googleapis.com/v1/mediaItems', headers=headers)
+    items = response.json().get('mediaItems', [])
+
+    nextPageToken = response.json().get('nextPageToken')
+
+    while nextPageToken:
+        response = requests.get(
+            f'https://photoslibrary.googleapis.com/v1/mediaItems?pageToken={nextPageToken}', headers=headers)
+        items.extend(response.json().get('mediaItems', []))
+        nextPageToken = response.json().get('nextPageToken')
 
     return items
 
@@ -134,16 +56,15 @@ def construct_graph(photos):
 
 def main():
     # Login to Google Photos
-    service = service_account_login()
-    if service is None:
-        print("Failed to log in to Google Photos.")
-        return
+    creds = service_account_login()
 
     # Get all photos
-    photos = get_all_photos(service)
+    photos = get_all_photos(creds)
+    print(f'Found {len(photos)} photos')
 
     # Construct graph
     G = construct_graph(photos)
+    print(f'Constructed graph with {len(G.nodes)} nodes and {len(G.edges)} edges')
 
 
 if __name__ == "__main__":
