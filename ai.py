26a51e5c52054931a28801d5e0e24d14d8b37c0d
Callum Sharrock <38599348+callum-sh@users.noreply.github.com>
2023-07-03 16:03:49 -0400

    working AI inference (kinda)

--- ai.py
+++ ai.py
@@ -1,47 +1,54 @@
+import torch
 import torchvision
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-from torchvision.transforms import functional as F
+import torchvision.transforms as transforms
 from PIL import Image
 
-# Load a pre-trained model
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-model.eval()
+# return what objects are in the image
 
-# Open your image
-image = Image.open('your_image.jpg')
 
-# Transform the image (resize, convert to tensor, normalize)
-transform = torchvision.transforms.Compose([
-    torchvision.transforms.ToTensor(),
-    torchvision.transforms.Normalize(
-        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
-image_tensor = transform(image)
+def get_objects(image_path):
+    # get the model
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
+        pretrained=True)
+    model.eval()
 
-# Make a prediction
-with torch.no_grad():
-    prediction = model([image_tensor])
+    # get the labels
+    labels = ['__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+              'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+              'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+              'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+              'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+              'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+              'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+              'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+              'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+              'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+              'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+              'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 
-# The prediction is a dict where 'boxes' contains the bounding boxes
-# and 'labels' contains the predicted labels (as numbers).
-print(prediction)
+    # Open the image
+    image = Image.open(image_path)
 
-# To convert label numbers to names, you can use the class names from the COCO dataset.
-COCO_INSTANCE_CATEGORY_NAMES = [
-    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
-    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
-    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
-    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
-    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
-    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
-    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
-    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
-]
+    # transform the image
+    transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
 
-# Print the predicted labels
-for label in prediction[0]['labels']:
-    print(COCO_INSTANCE_CATEGORY_NAMES[label])
+    # get the predictions
+    with torch.no_grad():
+        pred = model([transform(image).to('cpu')])[0]
+
+    # get the bounding boxes
+    boxes = pred['boxes'].tolist()
+    scores = pred['scores'].tolist()
+    pred_labels = [labels[i] for i in pred['labels'].tolist()]
+
+    # return the results
+    return boxes, scores, pred_labels
+
+
+if __name__ == "__main__":
+    boxes, scores, labels = get_objects('test.jpeg')
+    print(boxes)
+    print(scores)
+    print(labels)
