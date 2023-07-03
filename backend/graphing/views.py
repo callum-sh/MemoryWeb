c4c263eafb8c2f9f606501f90c1e5dfe381c8f66
Callum Sharrock <38599348+callum-sh@users.noreply.github.com>
2023-07-03 17:37:01 -0400

    basic file structure + model setup

--- backend/graphing/views.py
+++ backend/graphing/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
-
-import utils
-
-# Create your views here.
-def images(request):
-    imgs = utils.get_photos.get_all_photos()
+from django.core.files.storage import default_storage
+from rest_framework import viewsets, serializers
+from rest_framework.response import Response
+from .models import MyImageModel  # your image model here
 
 
+class ImageSerializer(serializers.ModelSerializer):
+    url = serializers.SerializerMethodField('get_image_url')
 
-    
+    class Meta:
+        model = MyImageModel  # your image model here
+        fields = ['url']  # and other fields of the model if any
+
+    def get_image_url(self, obj):
+        return obj.image.url  # change this if your image field has a different name
+
+
+class ImageViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = MyImageModel.objects.all()  # your image model here
+        serializer = ImageSerializer(queryset, many=True)
+        return Response(serializer.data)
