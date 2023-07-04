dd2b690a6aa1fd151f3436234fa8f1d33b629344
Josh Puglielli <joshpuglielli@pop-os.localdomain>
2023-07-04 16:11:07 -0400

    in circles now??

--- frontend/src/components/PhotoGallery.js
+++ frontend/src/components/PhotoGallery.js
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Card, Container, Row, Col } from 'react-bootstrap';
+import { Card, Container, Row, Col, Modal } from 'react-bootstrap';
 
 const PhotoGallery = () => {
   const [photos, setPhotos] = useState([]);
+  const [currentImage, setCurrentImage] = useState(null);
+
+  const handleImageClick = (photo) => {
+    setCurrentImage(photo);
+  }
+
+  const handleClose = () => {
+    setCurrentImage(null);
+  }
 
   useEffect(() => {
     axios
@@ -22,15 +31,38 @@ const PhotoGallery = () => {
         {photos.map(photo => (
           <Col md={4} key={photo.id}>
             <Card>
-              <Card.Img variant="top" src={photo.baseUrl} />
-              <Card.Body>
-                <Card.Title>{photo.filename}</Card.Title>
-                <Card.Text>{photo.productUrl}</Card.Text>
-              </Card.Body>
+              <div 
+                onClick={() => handleImageClick(photo)}
+                style={{ 
+                  width: '100px', 
+                  height: '100px', 
+                  borderRadius: '50%', 
+                  overflow: 'hidden', 
+                  display: 'flex', 
+                  justifyContent: 'center', 
+                  alignItems: 'center',
+                }}
+              >
+                <img 
+                  src={photo.baseUrl} 
+                  alt=""
+                  style={{
+                    flexShrink: 0,
+                    minWidth: '50%',
+                    minHeight: '50%',
+                  }} 
+                />
+              </div>
             </Card>
           </Col>
         ))}
       </Row>
+
+      <Modal show={currentImage !== null} onHide={handleClose}>
+        <Modal.Body>
+          {currentImage && <img src={currentImage.baseUrl} alt="" style={{width: '100%'}} />}
+        </Modal.Body>
+      </Modal>
     </Container>
   );
 };
